559098456e5220636384046c2fa26f7cf86fac7f
jyo217 <solbaram217@gmail.com>
2023-08-29 15:50:06 +0900

    Skill.cs 기본 틀 완성 #5

--- Character.cs
+++ Character.cs
@@ -9,19 +9,22 @@ namespace SpartaDungeonBattle
 {
     internal class Character
     {
-        private int Level;
-        private string Name;
-        private ClassType Class;
-        private List<Equipment> ItemOnEquipped;
-        private int Attack;
-        private int Defense;
-        private int HP;
-        private int MP;
-        private int Gold;
-        private int Exp;
-        private Inventory Inventory;
-        private List<Skill> Skills;
+        public int Level { get; private set; }
+        public string Name { get; private set; }
+        public ClassType Class { get; private set; }
+        public List<Equipment> ItemOnEquipped { get; private set; }
+        public int Attack { get; private set; }
+        public int Defense { get; private set; }
+        public int HP { get; private set; }
+        public int MP { get; private set; }
+        public int Gold { get; private set; }
+        public int Exp { get; private set; }
+        public Inventory Inventory { get; private set; }
+        public List<Skill> Skills { get; private set; }
 
+        /// <summary>
+        /// 필요에 따라 편집
+        /// </summary>
         public Character() { }
 
         public void UseSkill(Skill skill) { }
